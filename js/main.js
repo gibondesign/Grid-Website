174146eb907a64baa454edfb06e30512b364d7be
Gib-ON Design <gibonwroc@gmail.com>
2023-03-07 19:23:47 +0100

    fixed mobile navigation (js)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
+const allMobileNavLinks = document.querySelectorAll(".nav__link-mobile");
 const footerYear = document.querySelector(".footer__year");
-const footer = document.querySelector(".footer")
+const footer = document.querySelector(".footer");
 
 const handleNav = () => {
-    navBtn.classList.toggle("is-active")
-    navMobile.classList.toggle("nav-mobile--active")
-    footer.classList.toggle("footer--disable")
-}
+	navBtn.classList.toggle("is-active");
+	navMobile.classList.toggle("nav-mobile--active");
+	document.body.classList.toggle("sticky-body");
+};
 
-navBtn.addEventListener("click", handleNav)
+navBtn.addEventListener("click", handleNav);
 
-
+allMobileNavLinks.forEach((item) => {
+	item.addEventListener("click", () => {
+		navBtn.classList.remove("is-active");
+		navMobile.classList.remove("nav-mobile--active");
+		document.body.classList.remove("sticky-body");
+	});
+});
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
